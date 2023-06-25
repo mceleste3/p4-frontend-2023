e6ca96cd8a163f01abddf04ce715706d0413929d
María Celeste Ortiz <maria.celeste.ortz@gmail.com>
2023-06-25 12:48:28 +0200

    - Implementación del tipo Comic - Muesta de imágenes en la lista de cómics

--- src/components/ComicsList.tsx
+++ src/components/ComicsList.tsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 import Comic from "./Comic";
 
 function ComicsList() {
-  const [comicsList, setComicsList] = useState([]);
+  const [comicsList, setComicsList] = useState<Comic[]>([]);
+
+  interface Comic {
+    dates: Array<Object>;
+    description: string;
+    id: number;
+    isbn: string;
+    pageCount: number;
+    path: string;
+    extension: string;
+    title: string;
+  }
 
   const loadComics = async () => {
     const response = await fetch(
@@ -10,18 +21,35 @@ function ComicsList() {
         import.meta.env.VITE_APIKEY
       }&hash=${import.meta.env.VITE_HASH}`
     );
-    const result: any = await response.json();
-    const obj = result["data"]["results"];
-    console.log(obj.length);
-
-    setComicsList(obj);
+    const result = await response.json();
+    //console.log(result);
+    const comicsResults: Array<any> = result.data.results;
+    //console.log(comicsResults.length);
+    const comics: Array<Comic> = [];
+    comicsResults.forEach(elem => {
+      const comic: Comic = {
+        dates: elem.dates,
+        description: elem.description,
+        id: elem.id,
+        isbn: elem.isbn,
+        pageCount: elem.pageCount,
+        path: elem.thumbnail.path,
+        extension: elem.thumbnail.extension,
+        title: elem.title,
+      };
+      comics.push(comic);
+    }); 
+    console.log(comics);
+    //console.log(Object.keys(comics[0]));
+    setComicsList(comics);
   };
 
   useEffect(() => {
+    console.log("datos");
     loadComics();
   }, []);
 
-  if (comicsList.length === 0) {
+  if (comicsList.length === 1) {
     return <div>Loading...</div>;
   }
 
@@ -32,8 +60,8 @@ function ComicsList() {
       </h1>
       <hr className="border-yellow-600" />
       <div className="grid grid-cols-4 gap-5 mr-8 ml-8 mt-12 mb-16 pb-16 pt-9 bg-zinc-900 shadow-2xl shadow-zinc-900">
-        {comicsList.map((comic) => (
-          <Comic key={comic["id"]} title={comic["title"]} />
+        {comicsList.map((comic:Comic) => (
+          <Comic key={comic.id} title={comic.title} path={comic.path} extension={comic.extension} />
         ))}
       </div>
     </>
